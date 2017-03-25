19b22e47174f2d3624a9a779e034b67400b5f1eb
Benjozork <benjozork@icloud.com>
2017-03-25 15:49:32 -0400

    branch2 test

--- core/src/me/benjozork/onyx/screen/GameScreen.java
+++ core/src/me/benjozork/onyx/screen/GameScreen.java
@@ -83,7 +83,7 @@ public class GameScreen implements Screen {
 
           // Crossfade
           if (isFading) {
-               float totalFadeDelta;
+               float totalFadeDelta;aa
 
                deltaRed = fadeColors[fadeIndex].r - currentColor.r;
                deltaGreen = fadeColors[fadeIndex].g - currentColor.g;
