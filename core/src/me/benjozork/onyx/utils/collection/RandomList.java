38bd488a299c0b4c124504835af48e22fb76a177
Benjozork <benjozorkfr@gmail.com>
2017-03-26 15:35:12 -0400

    added RandomList

---
+++ core/src/me/benjozork/onyx/utils/collection/RandomList.java
@@ -0,0 +1,67 @@
+package me.benjozork.onyx.utils.collection;
+
+import com.badlogic.gdx.utils.Array;
+
+import java.util.Random;
+
+/**
+ * A list that will return a random item each time next() is called.
+ * @author Benjozork
+ * @param <T> the type of the items in the list
+ */
+public class RandomList<T> {
+
+    private final Array<T> items = new Array<T>();
+    private Random random = new Random();
+    private int currentIndex = 0;
+    private T prevItem;
+
+    private RandomList(T... items) {
+        for (T item : items) {
+            this.items.add(item);
+        }
+    }
+
+    private RandomList(Array<T> items) {
+        for (T item : items) {
+            this.items.add(item);
+        }
+    }
+
+    public static <T> RandomList<T> of(T... items) {
+        if (items == null || items.length == 0) {
+            throw new IllegalArgumentException("RandomList requires at least one item!");
+        }
+
+        return new RandomList<T>(items);
+    }
+
+    public static <T> RandomList<T> of(Array<T> items) {
+        if (items == null || items.size == 0) {
+            throw new IllegalArgumentException("RandomList requires at least one item!");
+        }
+
+        return new RandomList<T>(items);
+    }
+
+    public T next() {
+        prevItem = items.get(currentIndex);
+        int index = random.nextInt(items.size);
+        currentIndex = index;
+        return items.get(index);
+    }
+
+    public T next(boolean ignorePrevItem) {
+        int prev = currentIndex, index = 0;
+        do {
+            index = random.nextInt(items.size);
+        } while (prev == index);
+        currentIndex = index;
+        return items.get(index);
+    }
+
+    public T current() {
+        return items.get(currentIndex);
+    }
+
+}
