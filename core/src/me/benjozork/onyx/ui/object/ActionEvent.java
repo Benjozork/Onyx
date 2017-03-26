8bcd6a03c027260480f0cd71a16f00fa5724ec7f
Benjozork <benjozorkfr@gmail.com>
2017-03-26 11:11:00 -0400

    added ActionEvent javadocs

--- core/src/me/benjozork/onyx/ui/object/ActionEvent.java
+++ core/src/me/benjozork/onyx/ui/object/ActionEvent.java
@@ -3,6 +3,10 @@ package me.benjozork.onyx.ui.object;
 /**
  * Created by Benjozork on 2017-03-25.
  */
+
+/**
+ * An ActionEvent specifies what an Action should listen to.
+ */
 public enum ActionEvent {
     CLICKED,
     HOVERED,
