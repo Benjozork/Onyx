3436d921829c18cefaccb7ee7380e7faaa7a0c28
Benjozork <benjozorkfr@gmail.com>
2017-03-27 17:04:17 -0400

    removed unused imports
    Signed-off-by: Benjozork <benjozorkfr@gmail.com>

--- core/src/me/benjozork/onyx/entity/EntityProjectile.java
+++ core/src/me/benjozork/onyx/entity/EntityProjectile.java
@@ -3,7 +3,6 @@ package me.benjozork.onyx.entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
--- core/src/me/benjozork/onyx/ui/object/TextComponent.java
+++ core/src/me/benjozork/onyx/ui/object/TextComponent.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.sun.istack.internal.NotNull;
 
 /**
  * Created by Benjozork on 2017-03-25.
