c3f378161972612c33012a6087084d9971afb172
Benjozork <benjozorkfr@gmail.com>
2017-03-27 17:00:29 -0400

    made TextComponent able to generate its own fonts
    Signed-off-by: Benjozork <benjozorkfr@gmail.com>

--- core/src/me/benjozork/onyx/ui/object/TextComponent.java
+++ core/src/me/benjozork/onyx/ui/object/TextComponent.java
@@ -1,8 +1,10 @@
 package me.benjozork.onyx.ui.object;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.sun.istack.internal.NotNull;
 
 /**
  * Created by Benjozork on 2017-03-25.
@@ -11,11 +13,14 @@ public class TextComponent {
 
     private String text;
     private String fontPath;
+    private FreeTypeFontGenerator generator;
     private FreeTypeFontGenerator.FreeTypeFontParameter parameter;
     private GlyphLayout layout;
 
+    private BitmapFont generatedFont;
+
     /**
-     * A TextComponent defines the text component of an UIElement. A certain font path is specified along with a string and a FreeTypeFontParameter.
+     * A TextComponent defines a text that is drawn on the screen. A certain font path is specified along with a string and a FreeTypeFontParameter.
      * @param text the text to be displayed
      * @param fontPath the font path to be used
      * @param parameter the FreeTypeFontParameter to be used
@@ -23,8 +28,18 @@ public class TextComponent {
     public TextComponent(String text, String fontPath, FreeTypeFontGenerator.FreeTypeFontParameter parameter) {
         this.text = text;
         this.fontPath = fontPath;
+        this.generator = new FreeTypeFontGenerator(Gdx.files.internal(fontPath));
         this.parameter = parameter;
         this.layout = new GlyphLayout(new FreeTypeFontGenerator(Gdx.files.internal(fontPath)).generateFont(parameter), text);
+    }
+
+    public BitmapFont regenerateFont() {
+        generatedFont = generator.generateFont(parameter);
+        return generatedFont;
+    }
+
+    public BitmapFont getFont() {
+        return generatedFont;
     }
 
     public String getText() {
