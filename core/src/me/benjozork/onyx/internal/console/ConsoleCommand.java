748ef3455bf0689790f6f669c1916fd5e277da06
Benjozork <benjozorkfr@gmail.com>
2017-04-01 13:17:23 -0400

    Change ConsoleCommand constructor

--- core/src/me/benjozork/onyx/internal/console/ConsoleCommand.java
+++ core/src/me/benjozork/onyx/internal/console/ConsoleCommand.java
@@ -1,17 +1,26 @@
 package me.benjozork.onyx.internal.console;
 
 /**
+ * Describes a command sent to the console
  * @author Benjozork
  */
 public class ConsoleCommand {
 
-    public ConsoleCommand(String command, String... args) {
-        this.command = command;
-        this.args = args;
-    }
-
     private String command;
     private String args[];
+
+    /**
+     * @param str the whole string of the command, including identifier and arguments all separated by whitespaces
+     */
+    public ConsoleCommand(String str) {
+        String[] splitBuf = str.split(" ");
+        this.command = splitBuf[0];
+        if (splitBuf.length <= 1) {
+            this.args = new String[]{};
+            return;
+        }
+        System.arraycopy(splitBuf, 1, args, 1, splitBuf.length - 1);
+    }
 
     public String getCommand() {
         return command;
