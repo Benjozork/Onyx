6408e0a5167afc987e36b686c7a4006dff915f59
Benjozork <benjozorkfr@gmail.com>
2017-04-01 16:26:39 -0400

    Configs docs

--- core/src/me/benjozork/onyx/config/Configs.java
+++ core/src/me/benjozork/onyx/config/Configs.java
@@ -10,6 +10,10 @@ import me.benjozork.onyx.logger.Log;
 import me.benjozork.onyx.utils.Bounds;
 import me.benjozork.onyx.utils.Maybe;
 
+/**
+ * Manages the game's various configuration files
+ * @author angelickite
+ */
 public class Configs {
 
     private static final Log log = Log.create("Configs");
@@ -22,15 +26,32 @@ public class Configs {
         json.setSerializer(Bounds.class, new BoundsSerializer());
     }
 
+    /**
+     * Sets the {@link Files} object
+     * @param files the {@link Files} object to be used
+     */
     public static void setFiles(Files files) {
         Configs.files = files;
     }
 
+    /**
+     * Reloads a certain config file
+     * @param internalPath the path for the file to be reloaded from
+     * @param clazz the class of the config file that has to be reloaded
+     * @return the loaded file
+     */
     public static <T> Maybe<T> reload(String internalPath, Class<T> clazz) {
         cache.remove(internalPath);
         return load(internalPath, clazz);
     }
 
+    /**
+     * Loads a certain config file
+     *
+     * @param internalPath the path for the file to be loaded from
+     * @param clazz the class of the config file that has to be loaded
+     * @return the loaded file
+     */
     public static <T> Maybe<T> load(String internalPath, Class<T> clazz) {
         if (cache.containsKey(internalPath)) {
             return Maybe.of((T) cache.get(internalPath));
@@ -53,11 +74,29 @@ public class Configs {
         return Maybe.of(config);
     }
 
+    /**
+     * Reloads a certain config file, and throws an {@link IllegalStateException} if said file does not exist
+     *
+     * @param internalPath the path for the file to be reloaded from
+     * @param clazz the config file class that has to be reloaded
+     * @return the reloaded file
+     *
+     * @throws IllegalStateException if the file does not exist
+     */
     public static <T> T reloadRequire(String internalPath, Class<T> clazz) {
         cache.remove(internalPath);
         return loadRequire(internalPath, clazz);
     }
 
+    /**
+     * Loads a certain config file, and throws an {@link IllegalStateException} if said file does not exist
+     *
+     * @param internalPath the path for the file to be loaded from
+     * @param clazz the config file class that has to be loaded
+     * @return the loaded file
+     *
+     * @throws IllegalStateException if the file does not exist
+     */
     public static <T> T loadRequire(String internalPath, Class<T> clazz) {
         Maybe<T> configMaybe = load(internalPath, clazz);
 
@@ -69,11 +108,33 @@ public class Configs {
         return configMaybe.get();
     }
 
+    /**
+     * Reloads a certain config file, and tries to reload a fallback file if the former does not exist.
+     * Throws an {@link IllegalStateException} if the former also does not exist.
+     *
+     * @param internalPath the path for the file to be reloaded from
+     * @param fallbackInternalPath the path for the fallback file to be reloaded from, if the latter fails
+     * @param clazz the config file class that has to be reloaded
+     * @return the reloaded file
+     *
+     * @throws IllegalStateException if the fallback file does not exist
+     */
     public static <T> T reloadRequireWithFallback(String internalPath, String fallbackInternalPath, Class<T> clazz) {
         cache.remove(internalPath);
         return loadRequireWithFallback(internalPath, fallbackInternalPath, clazz);
     }
 
+    /**
+     * Loads a certain config file, and tries to load a fallback file if the former does not exist.
+     * Throws an {@link IllegalStateException} if the former also does not exist.
+     *
+     * @param internalPath the path for the file to be loaded from
+     * @param fallbackInternalPath the path for the fallback file to be loaded from, if the latter fails
+     * @param clazz the config file class that has to be loaded
+     * @return the loaded file
+     *
+     * @throws IllegalStateException if the fallback file does not exist
+     */
     public static <T> T loadRequireWithFallback(String internalPath, String fallbackInternalPath, Class<T> clazz) {
         Maybe<T> configMaybe = load(internalPath, clazz);
 
@@ -84,11 +145,27 @@ public class Configs {
         }
     }
 
+    /**
+     * Reloads a certain config file, and tries to reload a fallback file if the former does not exist.
+     *
+     * @param internalPath the path for the file to be reloaded from
+     * @param fallbackInternalPath the path for the fallback file to be reloaded from, if the latter fails
+     * @param clazz the config file class that has to be reloaded
+     * @return the reloaded file
+     */
     public static <T> Maybe<T> reloadWithFallback(String internalPath, String fallbackInternalPath, Class<T> clazz) {
         cache.remove(internalPath);
         return loadWithFallback(internalPath, fallbackInternalPath, clazz);
     }
 
+    /**
+     * Loads a certain config file, and tries to load a fallback file if the former does not exist.
+     *
+     * @param internalPath the path for the file to be loaded from
+     * @param fallbackInternalPath the path for the fallback file to be loaded from, if the latter fails
+     * @param clazz the class of the config file that has to be loaded
+     * @return the loaded file
+     */
     public static <T> Maybe<T> loadWithFallback(String internalPath, String fallbackInternalPath, Class<T> clazz) {
         Maybe<T> configMaybe = load(internalPath, clazz);
 
@@ -99,6 +176,9 @@ public class Configs {
         }
     }
 
+    /**
+     * Clears the file cache
+     */
     public static void clearCache() {
         cache.clear();
         int cacheSize = cache.size;
