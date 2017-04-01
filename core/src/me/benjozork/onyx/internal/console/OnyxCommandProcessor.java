afdfa3225885aac8515fe79e52212019ef1749cf
Benjozork <benjozorkfr@gmail.com>
2017-04-01 13:18:23 -0400

    Logger support in OnyxCommandProcessor

--- core/src/me/benjozork/onyx/internal/console/OnyxCommandProcessor.java
+++ core/src/me/benjozork/onyx/internal/console/OnyxCommandProcessor.java
@@ -1,28 +1,30 @@
 package me.benjozork.onyx.internal.console;
 
 import me.benjozork.onyx.internal.ScreenManager;
+import me.benjozork.onyx.logger.Log;
 import me.benjozork.onyx.screen.GameScreen;
 import me.benjozork.onyx.screen.MenuScreen;
-import me.benjozork.onyx.internal.Logger;
 
 /**
- * The default {@link CommandProcessor} fpr Onyx
+ * The default {@link CommandProcessor} for Onyx
  * @author Benjozork
  */
 public class OnyxCommandProcessor implements CommandProcessor {
+
+    private static Log log = Log.create("OnyxCommandProcessor");
 
     @Override
     public void onCommand(ConsoleCommand c) {
         if (c.getCommand().equals("screen")) {
             if (c.getArgs().length == 0) {
-                Logger.log("Need one argument: [game, menu]");
+                log.print("Need one argument: [game, menu]");
             } else {
                 if (c.getArgs()[0].equals("game")) {
                     ScreenManager.setCurrentScreen(new GameScreen());
                 } else if (c.getArgs()[0].equals("menu")) {
                     ScreenManager.setCurrentScreen(new MenuScreen());
                 } else {
-                    Logger.log("Invalid argument");
+                    log.print("Invalid argument '%s'", c.getArgs()[1]);
                 }
             }
         }
