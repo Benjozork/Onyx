914ebda7846661379b6beedf5e59b331f7bdd97a
Benjozork <benjozorkfr@gmail.com>
2017-04-01 17:14:36 -0400

    Maybe docs

--- core/src/me/benjozork/onyx/utils/Maybe.java
+++ core/src/me/benjozork/onyx/utils/Maybe.java
@@ -1,5 +1,10 @@
 package me.benjozork.onyx.utils;
 
+/**
+ * Represents an object that may or may not exist
+ * @param <T> the type of the object
+ * @author angelickite
+ */
 public class Maybe<T> {
 
     private final T value;
@@ -8,16 +13,28 @@ public class Maybe<T> {
         this.value = value;
     }
 
+    /**
+     * Returns an empty {@link Maybe}
+     * @param <T> the type of the object
+     */
     public static <T> Maybe<T> empty() {
         return new Maybe<T>(null);
     }
 
+    /**
+     * Returns a {@link Maybe} containing a corresponding object
+     * @param value the contained object
+     */
     public static <T> Maybe<T> of(T value) {
         if (value == null)
             throw new NullPointerException("value may not be null! use Maybe.empty() or Maybe.unsure(value) instead");
         return new Maybe<T>(value);
     }
 
+    /**
+     * Returns a {@link Maybe} that may or may not contain an object
+     * @param value the contained object
+     */
     public static <T> Maybe<T> unsure(T value) {
         return new Maybe<T>(value);
     }
@@ -26,6 +43,9 @@ public class Maybe<T> {
         return value;
     }
 
+    /**
+     * Checks whether the {@link Maybe} contains an object
+     */
     public boolean exists() {
         return value != null;
     }
