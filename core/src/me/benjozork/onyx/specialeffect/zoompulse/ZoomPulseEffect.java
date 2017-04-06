66c45a8fee9b032414d6a959da8af9cf5a3ffc71
Benjozork <benjozorkfr@gmail.com>
2017-04-06 18:35:32 -0400

    Fized ZoomPulseEffect

--- core/src/me/benjozork/onyx/specialeffect/zoompulse/ZoomPulseEffect.java
+++ core/src/me/benjozork/onyx/specialeffect/zoompulse/ZoomPulseEffect.java
@@ -3,10 +3,8 @@ package me.benjozork.onyx.specialeffect.zoompulse;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.Array;
 
-
 import me.benjozork.onyx.specialeffect.Effect;
 import me.benjozork.onyx.utils.Utils;
-
 
 /**
  * A ZoomPulseEffect allows creating a short zoom pulse effect according to the provided<br/>
@@ -26,17 +24,17 @@ public class ZoomPulseEffect extends Effect {
     private float zoomStep;
 
     /**
-     * @param configuration the {@link ZoomPulseEffectConfiguration} to use for the zoomback effect
-     * @param cameras       the cameras to apply the effect to
+     * @param configuration the {@link ZoomPulseEffectConfiguration} which drives the effect
+     * @param cameras       the cameras to be manipulated
      */
     public ZoomPulseEffect(ZoomPulseEffectConfiguration configuration, OrthographicCamera... cameras) {
-        if (cameras.length == 0) throw new IllegalArgumentException("at least one camera required");
+        if (cameras.length == 0) throw new IllegalArgumentException("at least one camera is required");
         this.cameras = Array.with(cameras);
         this.maxZoomTime = configuration.maxZoomTime / 6;
         this.targetZoom = configuration.targetZoom;
     }
 
-
+    @Override
     public void update() {
         if (! isActive) return;
 
@@ -56,10 +54,10 @@ public class ZoomPulseEffect extends Effect {
             zoomBack = true;
         }
 
-        for (OrthographicCamera cam : cameras) {
-            if (cam.zoom > 1f) {
-                for (OrthographicCamera cam2 : cameras) {
-                    cam2.zoom = 1f;
+        for (OrthographicCamera i : cameras) {
+            if (i.zoom > 1f) {
+                for (OrthographicCamera j : cameras) {
+                    j.zoom = 1f;
                 }
                 zoomBack = false;
                 isActive = false;
